8fe4e3923017d266297245597163b1179191488b
Harun Kilic <harunkilic@live.dk>
2022-10-02 21:21:12 +0200

    second page

---
+++ src/app/heroes/heroes.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from '@angular/core';
+import { Hero } from '../hero';
+import { HEROES } from '../mock-heroes';
+
+@Component({
+  selector: 'app-heroes',
+  templateUrl: './heroes.component.html',
+  styleUrls: ['./heroes.component.css'],
+})
+export class HeroesComponent implements OnInit {
+  heroes = HEROES;
+
+  constructor() {}
+
+  ngOnInit(): void {}
+}
